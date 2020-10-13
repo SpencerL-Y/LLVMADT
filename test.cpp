1ebad148b0e8286052bc6231a26c04d3820d488b
feng weizhi <fengwz17@outlook.com>
2020-10-13 14:51:36 +0800

    test file

---
+++ test.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+
+// clang++ -O0 test2.cpp -fno-discard-value-names -DLLVM_ENABLE_ASSERTIONS=On -emit-llvm -S -o test4.ll
+
+int main()
+{
+    int x, y;
+    while(1)
+    {
+        x = 1;
+        x = 6;
+        y = 1;
+        
+        while(x > 0)
+        {
+            x--;
+            if ( x <= 1)
+                y = 0;
+        }
+    }    
+    return 0;
+}
+
