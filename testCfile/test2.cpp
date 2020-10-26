302cf2c2144c91842b5b15bb56cf3d6f22b9401d
Xie Li <lixie15@mails.ucas.ac.cn>
2020-10-26 20:57:55 +0800

    add

--- testCfile/test2.cpp
+++ testCfile/test2.cpp
@@ -2,10 +2,9 @@
 // using namespace std;
 
 // clang++ -O0 test2.cpp -fno-discard-value-names -DLLVM_ENABLE_ASSERTIONS=On -emit-llvm -S -o test4.ll
-
+int x, y;
 int main()
-{
-    int x, y;
+{ 
     x = 1;
     y = 2;
     x = 6;
